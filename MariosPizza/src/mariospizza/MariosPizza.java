70fa1e2b7b21644c902c4895938e75ac55d927ad
Frederik Dahl <fdah@itu.dk>
2019-10-11 08:36:59 +0200

    Print 2

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -8,6 +8,8 @@ package mariospizza;
 public class MariosPizza {
     public static void main(String[] args) {
         System.out.println("Det her er en test");
+        
+        System.out.println("Detter er test nummer 2");
     }
   
     
