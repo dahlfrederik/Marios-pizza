e145389b6a6acf13232071faa47f083094e80827
Frederik Dahl <fdah@itu.dk>
2019-10-11 08:37:44 +0200

    Restored the code for example work

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -6,11 +6,7 @@ package mariospizza;
  * @author FrederikDahl
  */
 public class MariosPizza {
-    public static void main(String[] args) {
-        System.out.println("Det her er en test");
-        
-        System.out.println("Detter er test nummer 2");
-    }
+ 
   
     
 }
