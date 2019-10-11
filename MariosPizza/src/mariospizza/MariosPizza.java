5eb1d778700381968f8f165c6978c220b12d5349
Frederik Dahl <fdah@itu.dk>
2019-10-11 08:41:15 +0200

    Fixed thors rode kode

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -6,12 +6,6 @@ package mariospizza;
  * @author FrederikDahl
  */
 public class MariosPizza {
-    public static void main(String[] args) {
-        System.out.println("Det her er en test");
-        
-        System.out.println("Detter er test nummer 2");
-        System.out.println("asdasd");
-    }
  
     }
   
