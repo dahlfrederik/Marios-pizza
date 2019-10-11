8fd2b5a9211161bfa4940438d15e9a86d280816a
Frederik Dahl <fdah@itu.dk>
2019-10-11 08:38:54 +0200

    Example 2

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -6,7 +6,9 @@ package mariospizza;
  * @author FrederikDahl
  */
 public class MariosPizza {
- 
+    public static void main(String[] args) {
+        System.out.println("ÅH NEJ MERGE CONFLICT");
+    }
   
     
 }
