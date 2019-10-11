5f0a0a61adaae812c5bc7a9fbcbeaffbf38386ba
Frederik Dahl <fdah@itu.dk>
2019-10-11 08:31:29 +0200

    Added println

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -6,7 +6,9 @@ package mariospizza;
  * @author FrederikDahl
  */
 public class MariosPizza {
-    
+    public static void main(String[] args) {
+        System.out.println("Det her er en test");
+    }
   
     
 }
